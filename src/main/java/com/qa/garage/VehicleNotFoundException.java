2e8d5948f6ec531995fd3a590b08887646f10b3f
44jovi <kwong.jovi@gmail.com>
2023-03-21 11:31:21 +0000

    Progress on VehicleNotFoundException

--- src/main/java/com/qa/garage/VehicleNotFoundException.java
+++ src/main/java/com/qa/garage/VehicleNotFoundException.java
@@ -1,5 +1,12 @@
 package com.qa.garage;
 
 public class VehicleNotFoundException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3956396153639543220L;
 
+	public VehicleNotFoundException(String message) {
+		super(message);
+	}
 }
